e9af7b05f9260b848fb641e551733d0b7dc315a3
Ron Smith <ron@rpstechnologies.net>
2024-08-19 12:00:22 -0500

    new project + some user code

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+
+    // @Get()
+    // getHello(): string {
+    //   return this.appService.getHello();
+    // }
+  
+}
